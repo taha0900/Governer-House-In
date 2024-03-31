2b04346eaa48e064d841519eb76ce7ad5093d834
taha0900 <163208602+taha0900@users.noreply.github.com>
2024-04-01 02:30:35 +0500

    Add files via upload
    cli calculator

---
+++ index.js
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([
+    {
+        type: "number",
+        name: "numberOne",
+        message: "kindly enter your first no: "
+    },
+    {
+        type: "number",
+        name: "numberTwo",
+        message: "kindly enter your second no: "
+    },
+    {
+        type: "list",
+        name: "operator",
+        choices: ["*", "+", "-", "/"],
+        message: "select operator: "
+    },
+]);
+const { numberOne, numberTwo, operator } = answers;
+if (numberOne && numberTwo && operator) {
+    let result = 0;
+    if (operator === "+") {
+        result = numberOne + numberTwo;
+    }
+    else if (operator === "-") {
+        result = numberOne - numberTwo;
+    }
+    if (operator === "*") {
+        result = numberOne * numberTwo;
+    }
+    if (operator === "/") {
+        result = numberOne / numberTwo;
+    }
+    console.log("Your result is :", result);
+}
+else {
+    console.log("kindly enter valid input");
+}
